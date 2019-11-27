946e84208eb71f6d46bafe4dba8fc4cbb26096ca
Sereza7 <lucas.charp78@gmail.com>
2019-11-27 13:16:33 +0100

    First Draft of the dialogflow communication package
    In this package, there is still only one node, the "create entity type" one. This allows, with the right authorizations of the agent, to create entity types on the bot.

---
+++ DialogFlow_OAS_setup/create_entitytype.js
@@ -0,0 +1,69 @@
+/* eslint-env es6 */
+/* eslint-disable */
+var dialogflow = require('dialogflow');
+
+
+module.exports = function (RED) {
+
+    
+    function CreateEntityTypeNode(options) {
+        RED.nodes.createNode(this, options);
+        var node = this;
+        node.dialogflow = options.dialogflow;
+        
+        
+        
+        node.on('input', function (msg) {
+            if (msg.typeName!==undefined){
+                node.typeName=msg.typeName;
+            }
+            else{
+                console.error("There is no 'msg.typeName' parameter given in the input message.")
+            }
+            
+          var dialogFlowNode = RED.nodes.getNode(node.dialogflow);
+          var variable = node.variable;
+
+          var email = dialogFlowNode.credentials.email;
+          var privateKey = dialogFlowNode.credentials.privateKey;
+          var projectId = dialogFlowNode.credentials.projectId;
+
+          var entityTypesClient = new dialogflow.EntityTypesClient({
+            credentials: {
+              private_key: privateKey,
+              client_email: email
+            }
+          });
+            const formattedParent = entityTypesClient.projectAgentPath(projectId);
+            var entityType = {
+
+                displayName:node.typeName,
+                kind:1
+                
+            };
+            const request = {
+              parent: formattedParent,
+              entityType: entityType,
+            };
+            entityTypesClient.createEntityType(request)
+              .then(responses => {
+                const response = responses[0];
+                // doThingsWith(response)
+                console.log(`Created ${responses} entity type`);
+                msg.payload=responses;
+                node.send(msg);
+              })
+              .catch(err => {
+                console.error(err);
+                msg.payload=responses;
+                node.send(msg);
+              });
+            
+            
+        });
+       
+        
+    }
+    RED.nodes.registerType("create_entitytype",CreateEntityTypeNode);
+}
+
